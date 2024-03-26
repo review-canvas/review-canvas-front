a97d0603f7daab5b28af038e1f8f092864b650bb
박성제 <ab@kurone.dev>
2024-03-26 13:10:45 +0900

    chore: set tailwind plugin

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,9 +1,16 @@
 import type { Config } from 'tailwindcss';
+import typographyPlugin from '@tailwindcss/typography';
+import animationPlugin from 'tailwindcss-animate';
+import scrollbarPlugin from 'tailwind-scrollbar';
 
 const config: Omit<Config, 'content'> = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    typographyPlugin,
+    animationPlugin,
+    scrollbarPlugin
+  ],
 };
 export default config;
