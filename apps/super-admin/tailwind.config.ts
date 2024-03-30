53eaba0de7bc6c21e8a592f1b16e49eec0f553ce
박성제 <ab@kurone.dev>
2024-03-31 03:14:34 +0900

    style: fix eslint problems

--- apps/super-admin/tailwind.config.ts
+++ apps/super-admin/tailwind.config.ts
@@ -1,6 +1,7 @@
 // tailwind config is required for editor support
 
 import type { Config } from 'tailwindcss';
+
 import sharedConfig from '@review-canvas/tailwind-config';
 
 const config: Pick<Config, 'content' | 'presets'> = {
--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss';
+
 import sharedConfig from '@review-canvas/tailwind-config';
 
 const config: Pick<Config, 'prefix' | 'presets' | 'content'> = {
