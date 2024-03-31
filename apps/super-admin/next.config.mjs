adeb9dabeb55dd482084e0d29cc8f6f1abfed5f1
박성제 <ab@kurone.dev>
2024-03-31 17:50:05 +0900

    build: ui package rename to admin-ui

--- apps/super-admin/next.config.mjs
+++ apps/super-admin/next.config.mjs
@@ -3,7 +3,7 @@ import withTwin from './with-twin.mjs';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  transpilePackages: ['@review-canvas/ui'],
+  transpilePackages: ['@review-canvas/admin-ui'],
 };
 
 export default withTwin(config);
