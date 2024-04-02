391d863bb1d4de0379fd4c7cc029007b8038808b
박성제 <ab@kurone.dev>
2024-04-02 10:43:28 +0900

    build: remove daisyui

--- packages/config-tailwind/tailwind.config.ts
+++ packages/config-tailwind/tailwind.config.ts
@@ -1,12 +1,11 @@
 import type { Config } from 'tailwindcss';
 import animationPlugin from 'tailwindcss-animate';
 import scrollbarPlugin from 'tailwind-scrollbar';
-import daisyuiPlugin from 'daisyui';
 
 const config: Omit<Config, 'content'> = {
   theme: {
     extend: {},
   },
-  plugins: [animationPlugin, scrollbarPlugin, daisyuiPlugin],
+  plugins: [animationPlugin, scrollbarPlugin],
 };
 export default config;
