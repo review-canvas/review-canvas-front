cbccebc2bd16303f9f604bdc43cbf082ff745a6b
박성제 <ab@kurone.dev>
2024-04-02 12:20:09 +0900

    feat: standalone output 제거

--- apps/shop-admin/next.config.mjs
+++ apps/shop-admin/next.config.mjs
@@ -3,9 +3,7 @@ import withTwin from './with-twin.mjs';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'standalone',
   transpilePackages: ['@review-canvas/admin-ui']
 };
 
 export default withTwin(config);
-// export default config;
--- apps/super-admin/next.config.mjs
+++ apps/super-admin/next.config.mjs
@@ -3,7 +3,6 @@ import withTwin from './with-twin.mjs';
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  output: 'standalone',
   transpilePackages: ['@review-canvas/admin-ui'],
 };
 
