ccae77386dc65ea0bd3804eb1cce3fa23e6c02c2
TwoShot22 <mansoo0621@naver.com>
2024-04-08 03:27:38 +0900

    feat(RVCV-106): 회원가입에서 다루어지는 상태에 대한 타입을 전체적으로 정의한다

---
+++ apps/shop-admin/src/types/signup/index.ts
@@ -0,0 +1,40 @@
+export interface SignupReviewItemActiveData {
+  title: boolean;
+  author: boolean;
+  point: boolean;
+  media: boolean;
+  content: boolean;
+  createdAt: boolean;
+  updatedAt: boolean;
+}
+
+export interface SignupReviewThemeData {
+  themeId: number;
+}
+
+export interface SignupUserSettingData extends SignupReviewItemActiveData, SignupReviewThemeData {}
+
+export interface SignupUserInfoData {
+  email: string;
+  password: string;
+  name: string;
+  logoImageUrl: string;
+  mallNumber: string;
+  phoneNumber: string;
+}
+
+export interface SignupData {
+  setting: SignupUserSettingData;
+  info: SignupUserInfoData;
+}
+
+export interface SignupStore {
+  currentStep: string;
+  steps: string[];
+  formData: SignupData;
+
+  setCurrentStep: (step: string) => void;
+  goToNextStep: () => void;
+  goToPrevStep: () => void;
+  updateFormData: (newData: Partial<SignupData>) => void;
+}
