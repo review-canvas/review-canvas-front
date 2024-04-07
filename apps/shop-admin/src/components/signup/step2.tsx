ccb3d574b6225fe949a59c2efc36e79d4f21a855
TwoShot22 <mansoo0621@naver.com>
2024-04-08 04:20:09 +0900

    feat(RVCV-106): 회원가입 2단계에서 디자인 테마 설정값을 store에 업데이트 하도록 구현한다

--- apps/shop-admin/src/components/signup/step2.tsx
+++ apps/shop-admin/src/components/signup/step2.tsx
@@ -1,19 +1,51 @@
-'use client'
+'use client';
 
-import useSignupStore from "@/store/signup";
+import useSignupStore from '@/store/signup';
+import { RadioGroupItem } from '@/types/components/radiogroup';
+import RadioGroup from '../common/radiogroup';
+import { useState } from 'react';
 
 function Step2() {
-    const { goToNextStep } = useSignupStore();
+  const themeArr: RadioGroupItem[] = [
+    {
+      id: '1',
+      name: '테마 1',
+    },
+    {
+      id: '2',
+      name: '테마 222',
+    },
+  ];
 
-    return (
-        <div>
-            <div>상품 상세 페이지의 리뷰 리스트 디자인 테마 선택</div>
-            <div>RadioGroup</div>
-            <div>
-                <button onClick={goToNextStep} type="button">다음 단계로 이동</button>
-            </div>
-        </div>
-    )
+  const [themeId, setThemeId] = useState(themeArr[0].id);
+  const { goToNextStep, updateSettingThemeId } = useSignupStore();
+
+  const onClickNextStepButton = () => {
+    updateSettingThemeId(Number(themeId))
+    goToNextStep()
+  }
+
+  return (
+    <div>
+      <div>상품 상세 페이지의 리뷰 리스트 디자인 테마 선택</div>
+      <div>RadioGroup</div>
+      <div>
+        <RadioGroup
+          items={themeArr}
+          selectedId={themeId}
+          onSelectedIdChange={setThemeId}
+        />
+      </div>
+      <div>
+        <button
+          onClick={onClickNextStepButton}
+          type="button"
+        >
+          다음 단계로 이동
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Step2
+export default Step2;
