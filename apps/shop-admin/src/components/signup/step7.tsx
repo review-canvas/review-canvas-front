0b7d68d2674fb02d3818fd27d50460dea154726e
TwoShot22 <mansoo0621@naver.com>
2024-04-08 02:13:44 +0900

    feat(RVCV-106): 회원가입 마지막 단계에서 로그인 페이지로 라우팅하는 로직을 버튼에 연동한다

--- apps/shop-admin/src/components/signup/step7.tsx
+++ apps/shop-admin/src/components/signup/step7.tsx
@@ -1,11 +1,17 @@
 'use client'
 
+import { useRouter } from 'next/navigation'
+
 function Step7() {
+    const router = useRouter()
+
     return (
         <div>
             <div>회원가입이 완료되었습니다</div>
             <div>
-                <button type="button">로그인 페이지로 이동</button>
+                <button onClick={() => {
+                    router.replace('/auth/login')
+                }} type="button">로그인 페이지로 이동</button>
             </div>
         </div>
     )
