993485681893d387db479d1be13914615b1598ed
TwoShot22 <mansoo0621@naver.com>
2024-04-08 01:36:42 +0900

    feat(RVCV-106): 회원가입 시 단계를 관리하는 store를 생성한다

---
+++ apps/shop-admin/src/store/signup/index.ts
@@ -0,0 +1,40 @@
+import { create } from 'zustand';
+
+interface SignupStore {
+  currentStep: string;
+  steps: string[];
+
+  setCurrentStep: (step: string) => void;
+  goToNextStep: () => void;
+  goToPrevStep: () => void;
+}
+
+const useSignupStore = create<SignupStore>((set) => ({
+  currentStep: 'step1',
+  steps: ['step1', 'step2', 'step3', 'step4', 'step5', 'step6'],
+  setCurrentStep: (step) => {
+    set({ currentStep: step });
+  },
+  goToNextStep: () => {
+    set((state) => {
+      const nextStepIndex = state.steps.indexOf(state.currentStep) + 1;
+
+      if (nextStepIndex < state.steps.length) {
+        return { currentStep: state.steps[nextStepIndex] };
+      }
+      return {};
+    });
+  },
+  goToPrevStep: () => {
+    set((state) => {
+      const prevStepIndex = state.steps.indexOf(state.currentStep) - 1;
+
+      if (prevStepIndex >= 0) {
+        return { currentStep: state.steps[prevStepIndex] };
+      }
+      return {};
+    });
+  },
+}));
+
+export default useSignupStore;
