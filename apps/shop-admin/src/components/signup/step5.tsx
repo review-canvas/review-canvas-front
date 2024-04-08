c2d4712d7196081aa2e328e03b8ad03e4cd5cf9a
TwoShot22 <mansoo0621@naver.com>
2024-04-08 17:03:43 +0900

    feat(RVCV-106): 회원가입 5단계를 구현한다

--- apps/shop-admin/src/components/signup/step5.tsx
+++ apps/shop-admin/src/components/signup/step5.tsx
@@ -1,19 +1,76 @@
-'use client'
+'use client';
 
-import useSignupStore from "@/store/signup";
+import type { RadioGroupItem } from '@/types/components/radiogroup';
+import type { SignupUserInstallData } from '@/types/signup';
 
-function Step5() {
-    const { goToNextStep } = useSignupStore();
+import { useState, useRef } from 'react';
 
-    return (
-        <div>
-            <div>리뷰캔버스 설치 방식을 지정해 주세요</div>
-            <div>RadioGroup</div>
-            <div>
-                <button onClick={goToNextStep} type="button">다음 단계로 이동</button>
-            </div>
-        </div>
-    )
+import RadioGroup from '@/components/common/radiogroup';
+import useSignupStore from '@/store/signup';
+
+interface InstallRadioGroupItem extends RadioGroupItem {
+  id: SignupUserInstallData['type'];
 }
 
-export default Step5
+function Step5() {
+  const installArr: InstallRadioGroupItem[] = [
+    {
+      id: 'ASK',
+      name: '설치 의뢰',
+    },
+    {
+      id: 'MANUAL',
+      name: '직접 설치',
+    },
+  ];
+  const [installType, setInstallType] = useState<SignupUserInstallData['type']>(installArr[0].id);
+  const askDetailRef = useRef<HTMLTextAreaElement>(null);
+
+  const { goToNextStep, updateReviewInstallData } = useSignupStore();
+
+  const onClickNextStepButton = () => {
+    const data: SignupUserInstallData = {
+      type: installType,
+    };
+
+    if (installType === 'ASK' && askDetailRef.current) {
+      data.askDetail = askDetailRef.current.value;
+    }
+
+    updateReviewInstallData(data);
+    goToNextStep()
+  };
+
+  return (
+    <div>
+      <div>리뷰캔버스 설치 방식을 지정해 주세요</div>
+      <div>
+        <RadioGroup
+          items={installArr}
+          selectedId={installType}
+          onSelectedIdChange={setInstallType as (newSelectedId: string) => void}
+        />
+      </div>
+
+      <div>
+        {installType === 'ASK' && (
+          <textarea
+            ref={askDetailRef}
+            placeholder="설치 의뢰와 관련해 자세한 정보를 적어 주세요"
+          />
+        )}
+      </div>
+
+      <div>
+        <button
+          onClick={onClickNextStepButton}
+          type="button"
+        >
+          다음 단계로 이동
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Step5;
