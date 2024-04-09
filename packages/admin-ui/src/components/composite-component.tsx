dbf4cc815fd9e0762476a4e21a1960752f9c1d8f
박성제 <ab@kurone.dev>
2024-04-09 16:43:22 +0900

    chore(RVCV-69): eslint rule disabled 제거

--- packages/admin-ui/src/components/composite-component.tsx
+++ packages/admin-ui/src/components/composite-component.tsx
@@ -1,15 +1,18 @@
 type ComponentMap<T> = {
-  [K in keyof T]: React.FC<T[K]>
-}
-type CompositeComponent<P, T> = React.FC<P> & ComponentMap<T>
+  [K in keyof T]: React.FC<T[K]>;
+};
+type CompositeComponent<P, T> = React.FC<P> & ComponentMap<T>;
 
-const createCompositeComponent = <P, T>(component: React.FC<P>, components: ComponentMap<T>, baseName?: string): CompositeComponent<P, T> => {
+const createCompositeComponent = <P, T>(
+  component: React.FC<P>,
+  components: ComponentMap<T>,
+  baseName?: string,
+): CompositeComponent<P, T> => {
   component.displayName ??= baseName;
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access -- 타입 추론 가능
   Object.entries(components).forEach(([k, v]) => {
     (v as React.FC).displayName ??= `${baseName}.${k}`;
   });
   return Object.assign(component, components);
-}
+};
 
 export default createCompositeComponent;
