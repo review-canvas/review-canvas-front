38d52d570f5e14dbdc7a2af50aee6e0a01dd24fd
박성제 <ab@kurone.dev>
2024-04-09 16:42:45 +0900

    chore(RVCV-69): 없어진 컴포넌트 삭제

--- apps/super-admin/src/app/page.tsx
+++ apps/super-admin/src/app/page.tsx
@@ -1,11 +1,5 @@
 'use client';
 
-import Button from '@review-canvas/admin-ui/button';
-
 export default function Page(): React.ReactNode {
-  return (
-    <main>
-      <Button onClick={() => {}}>test</Button>
-    </main>
-  );
+  return <main />;
 }
