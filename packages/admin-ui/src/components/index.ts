d7431bd56b886401457971df00ca07380b7715ef
박성제 <ab@kurone.dev>
2024-04-09 15:55:45 +0900

    feat(RVCV-69): 스위치 추가

--- packages/admin-ui/src/components/index.ts
+++ packages/admin-ui/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Checkbox } from './checkbox.tsx';
 export { default as RadioGroup } from './radio.tsx';
 export { default as ToggleButton } from './toggle-button.tsx';
+export { default as Switch } from './switch.tsx';
