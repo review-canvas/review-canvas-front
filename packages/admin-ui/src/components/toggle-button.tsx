316871fe54a0178d758cdd8cafdf5fde14655f0b
박성제 <ab@kurone.dev>
2024-04-09 16:17:36 +0900

    refactor(RVCV-69): 별도의 컴포넌트를 만들지 않고 styled component를 리턴하도록 수정

--- packages/admin-ui/src/components/toggle-button.tsx
+++ packages/admin-ui/src/components/toggle-button.tsx
@@ -1,23 +1,19 @@
-import { ToggleButton as AriaToggleButton, type ToggleButtonProps } from 'react-aria-components';
+import { ToggleButton as AriaToggleButton } from 'react-aria-components';
 
 import tw, { styled } from 'twin.macro';
 
-type Props = ToggleButtonProps & {
-  className?: string;
-};
-
-export default function ToggleButton({ children, ...props }: Props) {
-  return <StyledAriaToggleButton {...props}>{children}</StyledAriaToggleButton>;
-}
-
-const StyledAriaToggleButton = styled(AriaToggleButton)`
+const ToggleButton = styled(AriaToggleButton)`
   ${tw`rounded-5 flex justify-center items-center`};
   border: 1px solid ${({ theme }) => theme.colors.sub.primary};
   color: #838383;
-  transition: border-color 75ms linear, color 75ms linear;
-  
+  transition:
+    border-color 125ms linear,
+    color 125ms linear;
+
   &[data-selected] {
     border-color: ${({ theme }) => theme.colors.main.primary};
     color: ${({ theme }) => theme.colors.main.primary};
   }
 `;
+
+export default ToggleButton;
