1fb61cdbac79195e9c4f1d0dc5a220c074b18c8c
TwoShot22 <mansoo0621@naver.com>
2024-04-27 03:05:42 +0900

    feat: api의 request와 response, endpoint를 구조적으로 관리할 수 있도록 ApiService 클래스를 구현한다

---
+++ apps/shop-admin/src/lib/api/api-services.ts
@@ -0,0 +1,31 @@
+import type * as API from './api-types';
+
+import useTokenStore from '@/store/auth/token';
+
+import { HttpClient } from './http-client';
+
+class ApiService {
+  private httpClient: HttpClient;
+
+  constructor(httpClient: HttpClient) {
+    this.httpClient = httpClient;
+  }
+
+  public async postAuthLogin(request: API.PostAuthLoginRequest): Promise<API.PostAuthLoginResponse> {
+    const response = await this.httpClient.post<API.PostAuthLoginResponse>('/api/v1/shop-admin/login', {
+      email: request.email,
+      password: request.password,
+    });
+
+    return response.data;
+  }
+}
+
+const httpClient = new HttpClient();
+
+const { accessToken } = useTokenStore.getState();
+if (accessToken) {
+  httpClient.setAccessToken(accessToken);
+}
+
+export const apiService = new ApiService(httpClient);
---
+++ apps/shop-admin/src/lib/api/api-types.ts
@@ -0,0 +1,10 @@
+export interface PostAuthLoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface PostAuthLoginResponse {
+  adminId: number;
+  accessToken: string;
+  refreshToken: string;
+}
