2664607a8e4505d005ddf4f84ce74c26806a417d
wodnd0131 <wodnd0131@naver.com>
2024-04-30 18:10:05 +0900

    feat: 대시보드 메뉴 매핑 구조를 구현 및 추가한다.

---
+++ apps/super-admin/src/constants/menu/sub-menu.ts
@@ -0,0 +1,16 @@
+interface AdminSubMenuItem {
+  key: string;
+  name: string;
+  pathname?: string;
+  action?: () => void;
+}
+
+export const ADMIN_SUB_MENU_MAP: AdminSubMenuItem[] = [
+  {
+    key: 'logout',
+    name: '로그아웃',
+    action: () => {
+      return false;
+    },
+  },
+];
