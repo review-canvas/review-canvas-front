0e2bc1b3a6a265a96cf0dc87e8ce12719beadf04
wodnd0131 <wodnd0131@naver.com>
2024-04-30 17:28:32 +0900

    feat: 글로벌 스타일로 focus-visible가 없도록 추가한다.

--- apps/super-admin/src/components/global-styles.tsx
+++ apps/super-admin/src/components/global-styles.tsx
@@ -1,11 +1,13 @@
 'use client';
 
-import React from 'react';
-
 import { Global } from '@emotion/react';
 import { css, GlobalStyles as BaseStyles } from 'twin.macro';
 
-const customStyles = css({});
+const customStyles = css`
+  :focus-visible {
+    outline: none;
+  }
+`;
 
 function GlobalStyles() {
   return (
