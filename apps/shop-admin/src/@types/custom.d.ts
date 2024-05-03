923e3100bc807d7bd20a57b5b8fb6f1b02a3d8f7
wodnd0131 <wodnd0131@naver.com>
2024-05-04 00:17:22 +0900

    refactor: custom.d.ts 의 react import 방식을 CommonJS에서 ES6 방식으로 수정한다.

--- apps/shop-admin/src/@types/custom.d.ts
+++ apps/shop-admin/src/@types/custom.d.ts
@@ -1,5 +1,5 @@
 declare module '*.svg' {
-  import React = require('react');
+  import React from 'react';
 
   export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
   const src: string;
