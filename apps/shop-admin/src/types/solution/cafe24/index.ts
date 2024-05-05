d6b9020ef05f6c72716bc983b9dff8fe5f54ac31
TwoShot22 <mansoo0621@naver.com>
2024-05-06 00:54:54 +0900

    feat: Cafe24 App 설치 상태에 대한 타입을 새롭게 정의한다

--- apps/shop-admin/src/types/solution/cafe24/index.ts
+++ apps/shop-admin/src/types/solution/cafe24/index.ts
@@ -1,3 +1,5 @@
+export type SolutionCafe24InstallStatus = 'INSTALLED' | 'PREVIOUS_INSTALLED' | 'REGISTERED';
+
 export interface SolutionCafe24Store {
   mallId: string | null;
   setMallId: (_mallId: string | null) => void;
