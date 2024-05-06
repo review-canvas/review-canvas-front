bc19775b0a2678848befdd7ef6469ce04a6a7b7d
TwoShot22 <mansoo0621@naver.com>
2024-05-06 20:25:57 +0900

    feat: 회원가입 2단계에서 쓰이는 이메일 중복 체크 버튼을 컴포넌트로 분리한다

---
+++ apps/shop-admin/src/components/signup/email-check-button.tsx
@@ -0,0 +1,81 @@
+import type { SizeLevel } from '@ui/types/theme';
+
+import { SolidButton } from '@ui/components';
+
+import { AuthService } from '@/service/auth';
+import type { EmailCheckStatus } from '@/types/signup';
+
+interface EmailCheckButtonProps {
+  email: string;
+  status: EmailCheckStatus;
+  size?: SizeLevel;
+  isDisabled?: boolean;
+  onSetStatus: (status: EmailCheckStatus) => void;
+}
+
+type DuplicateCheckButtonProps = Required<Omit<EmailCheckButtonProps, 'status'>>;
+type DuplicateCheckCompleteButtonProps = Required<Pick<EmailCheckButtonProps, 'size'>>;
+
+function DuplicateCheckButton({ email, size, isDisabled, onSetStatus }: DuplicateCheckButtonProps) {
+  const handlePress = async () => {
+    try {
+      const isDuplicate = await AuthService.isEmailDuplicate(email);
+      onSetStatus(isDuplicate ? 'duplicate' : 'checked');
+
+      if (isDuplicate) {
+        // eslint-disable-next-line no-alert -- requre alert
+        alert('이미 존재하는 이메일입니다. 다른 이메일로 다시 시도해 주세요.');
+      }
+    } catch (err) {
+      // eslint-disable-next-line no-console -- track stace
+      console.error(err);
+      // eslint-disable-next-line no-alert -- require alert
+      alert('일시적으로 서버에 문제가 발생했습니다. 잠시 후 다시 시도해 주세요.');
+    }
+  };
+
+  return (
+    <SolidButton
+      variant="primary"
+      size={size}
+      isDisabled={isDisabled}
+      onPress={() => {
+        void handlePress();
+      }}
+    >
+      중복 확인
+    </SolidButton>
+  );
+}
+
+function DuplicateCheckCompleteButton({ size }: DuplicateCheckCompleteButtonProps) {
+  return (
+    <SolidButton
+      variant="gray"
+      size={size}
+      isDisabled
+    >
+      확인 완료
+    </SolidButton>
+  );
+}
+
+function EmailCheckButton({ email, status, size = 'sm', isDisabled = false, onSetStatus }: EmailCheckButtonProps) {
+  switch (status) {
+    case 'unchecked':
+    case 'duplicate':
+      return (
+        <DuplicateCheckButton
+          email={email}
+          size={size}
+          isDisabled={isDisabled}
+          onSetStatus={onSetStatus}
+        />
+      );
+
+    case 'checked':
+      return <DuplicateCheckCompleteButton size={size} />;
+  }
+}
+
+export default EmailCheckButton;
