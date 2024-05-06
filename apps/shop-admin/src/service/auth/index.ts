5a8cc4f6c2df4145499786642a85c0d4b2e95de1
TwoShot22 <mansoo0621@naver.com>
2024-05-06 20:25:31 +0900

    feat: AuthService에 회원가입 서비스 로직을 정의한다

--- apps/shop-admin/src/service/auth/index.ts
+++ apps/shop-admin/src/service/auth/index.ts
@@ -1,5 +1,6 @@
 import { apiService } from '@/lib/api/api-services';
 import useTokenStore from '@/store/auth/token';
+import type { SignupFormData } from '@/types/signup';
 
 async function login(email: string, password: string): Promise<void> {
   try {
@@ -19,7 +20,16 @@ async function isEmailDuplicate(email: string): Promise<boolean> {
   }
 }
 
+async function signup(formData: SignupFormData): Promise<void> {
+  try {
+    await apiService.postShopAdminSignUp(formData);
+  } catch (error) {
+    throw new Error('회원가입에 실패했습니다', error as ErrorOptions);
+  }
+}
+
 export const AuthService = {
   login,
   isEmailDuplicate,
+  signup,
 };
