da51ca3401ff4bcfcec68f2d176bd235c469f448
TwoShot22 <mansoo0621@naver.com>
2024-05-12 03:50:55 +0900

    feat: token store에 sessionStoarge persist를 적용한다

--- apps/shop-admin/src/store/auth/token.ts
+++ apps/shop-admin/src/store/auth/token.ts
@@ -1,12 +1,21 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 import type { TokenStore } from '@/types/auth/token';
 
-const useTokenStore = create<TokenStore>((set) => ({
-  accessToken: null,
-  setAccessToken: (_accessToken) => {
-    set({ accessToken: _accessToken });
-  },
-}));
+const useTokenStore = create(
+  persist<TokenStore>(
+    (set) => ({
+      accessToken: null,
+      setAccessToken: (_accessToken) => {
+        set({ accessToken: _accessToken });
+      },
+    }),
+    {
+      name: 'access-token',
+      storage: createJSONStorage(() => sessionStorage),
+    },
+  ),
+);
 
 export default useTokenStore;
