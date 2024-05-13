4811523c753e0c3b15f72a37202e4b6ac0828e9c
TwoShot22 <mansoo0621@naver.com>
2024-05-13 11:20:00 +0900

    feat: 요구사항에 맞춰 Popover로 Color Picker를 쓸 수 있도록 기획된 디자인에 맞춰 구현한다

--- packages/admin-ui/src/components/color-picker.tsx
+++ packages/admin-ui/src/components/color-picker.tsx
@@ -1,6 +1,8 @@
+import { Button, DialogTrigger, Popover } from 'react-aria-components';
 import type { IColor } from 'react-color-palette';
 import { ColorPicker as ReactColorPalettePicker, useColor } from 'react-color-palette';
 import 'react-color-palette/css';
+import { css, styled } from 'twin.macro';
 
 interface ColorPickerProps {
   color: string;
@@ -16,10 +18,44 @@ export default function ColorPicker({ color, onChange }: ColorPickerProps) {
   };
 
   return (
-    <ReactColorPalettePicker
-      color={convertedColor}
-      onChange={handleChange}
-      hideInput={['rgb', 'hsv']}
-    />
+    <DialogTrigger>
+      <StyledButton>
+        <ColorIndicator color={convertedColor.hex} />
+        <span className="label">색상 선택</span>
+      </StyledButton>
+      <Popover>
+        <ReactColorPalettePicker
+          color={convertedColor}
+          onChange={handleChange}
+          hideInput={['rgb', 'hsv']}
+        />
+      </Popover>
+    </DialogTrigger>
   );
 }
+
+const StyledButton = styled(Button)`
+  display: inline-flex;
+  align-items: center;
+  height: 35px;
+  border: 1px solid #b5b8d0;
+  border-radius: 5px;
+
+  .label {
+    padding: 0 14px;
+    text-align: center;
+    font-size: 0.75rem;
+    line-height: 1rem;
+    color: #adadad;
+  }
+`;
+
+const ColorIndicator = styled.div<{ color: string }>`
+  width: 35px;
+  height: 100%;
+  border-radius: 5px 0 0 5px;
+
+  ${({ color }) => css`
+    background-color: ${color};
+  `}
+`;
