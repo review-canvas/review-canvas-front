f72944d6797dd2a97c74a1a04e4fb31b96629ca0
TwoShot22 <mansoo0621@naver.com>
2024-05-26 01:38:08 +0900

    feat: Review Data와 관련된 공통 타입들을 정의한다

---
+++ apps/shop-admin/src/types/review/index.ts
@@ -0,0 +1,45 @@
+export type ReviewSortingType = 'LATEST' | 'HIGH_SCORE' | 'LOW_SCORE';
+
+export interface ReviewReplyDataType {
+  replyId: number;
+  content: string;
+  createAt: string;
+  updatedAt: string;
+  deleted: boolean;
+  userId: number;
+  nickname: string;
+}
+
+export interface ReviewDataType {
+  reviewId: number;
+  content: string;
+  score: number;
+  userId: number;
+  shopAdminId: number;
+  nickname: string;
+  isMine: boolean;
+  createAt: string;
+  updatedAt: string;
+  deleted: boolean;
+  replies: ReviewReplyDataType[];
+}
+
+export interface ReviewDataListType {
+  page: number;
+  size: number;
+  total: number;
+  content: ReviewDataType[];
+}
+
+export interface ProductDataType {
+  productId: number;
+  productNo: number;
+  productName: string;
+}
+
+export interface ProductDataListType {
+  page: number;
+  size: number;
+  total: number;
+  content: ProductDataType[];
+}
