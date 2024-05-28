b0cdd35f818eb8d0fbdf5ac154987f393d939b70
wodnd0131 <wodnd0131@naver.com>
2024-05-28 15:25:26 +0900

    feat:리플 수정,삭제 버튼 추가

--- apps/web/src/components/review/reply.tsx
+++ apps/web/src/components/review/reply.tsx
@@ -10,10 +10,27 @@ import {
 } from '@review-canvas/theme';
 
 import { useReviewItemStyle } from '@/contexts/style/review-item.ts';
+import useMessageToShop from '@/hooks/use-message-to-shop';
 import type { ReplyItem } from '@/services/api-types/review';
+import { MESSAGE_TYPES } from '@/utils/message';
 
-export default function Reply(props: ReplyItem) {
+interface ReplyItemProps {
+  reply: ReplyItem;
+}
+
+export default function Reply(props: ReplyItemProps) {
   const style = useReviewItemStyle();
+  const message = useMessageToShop();
+
+  const edit = () => {
+    message(MESSAGE_TYPES.OPEN_MODAL, {
+      type: 'edit',
+      url: `/replies/${props.reply.replyId}/edit`,
+    });
+  };
+
+  const deleteReply = () => {};
+
   return (
     <li
       css={[
@@ -31,11 +48,35 @@ export default function Reply(props: ReplyItem) {
         `,
       ]}
     >
-      <div>
+      <div className="relative">
         <div className="w-fit">
-          작성자 <span>{props.nickname}</span>
+          작성자 <span>{props.reply.nickname}</span>
         </div>
-        <p className="text-left">{props.content}</p>
+        <p className="text-left">{props.reply.content}</p>
+        {/*eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions --
+        This is intentional*/}
+        <div
+          className="absolute top-2 right-1 z-5"
+          hidden={props.reply.isMine}
+          onClick={(evt) => {
+            evt.stopPropagation();
+          }}
+        >
+          <button
+            className="border-b-2 border-gray-600/70 text-gray-700/90 mx-1"
+            onClick={edit}
+            type="button"
+          >
+            수정
+          </button>
+          <button
+            className="border-b-2 border-gray-600/70 text-gray-700/90 mx-1"
+            onClick={deleteReply}
+            type="button"
+          >
+            삭제
+          </button>
+        </div>
       </div>
     </li>
   );
