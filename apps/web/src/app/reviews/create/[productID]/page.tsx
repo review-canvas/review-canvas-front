c1c05d901ecacc0dc7f41709ac11e4ef9eba1af3
wodnd0131 <wodnd0131@naver.com>
2024-05-28 09:31:05 +0900

    refactor:리뷰 등록 데모 피드백 반영

--- apps/web/src/app/reviews/create/[productID]/page.tsx
+++ apps/web/src/app/reviews/create/[productID]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { useMutation } from '@tanstack/react-query';
 import { useParams } from 'next/navigation';
@@ -8,6 +8,7 @@ import { useParams } from 'next/navigation';
 import CloseButton from '@/components/close-button';
 import type { UploadImagesState } from '@/components/review/image-uploder';
 import { ImageUploader } from '@/components/review/image-uploder';
+import { Star } from '@/components/review/star';
 import useReviewCanvasReady from '@/hooks/use-review-canvas-ready.ts';
 import type { CreateReivewPathInfo, CreateReviewItemRequest } from '@/services/api-types/review';
 import { useReviewService } from '@/services/review';
@@ -24,7 +25,9 @@ export default function MyReviewsPage() {
     imageUrls: [],
   });
   const [content, setContent] = useState('');
-  const [score, setScore] = useState(1);
+  const [score, setScore] = useState(5);
+
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const shop = useShop();
   const params = useParams<PageParams>();
 
@@ -38,7 +41,6 @@ export default function MyReviewsPage() {
 
   useReviewCanvasReady('craete_review');
   const reviewService = useReviewService();
-
   const createReviewMutation = useMutation({
     mutationFn: async () => {
       await reviewService.create(pathInfo, ReviewItemRequest, uploadImages.imageFiles);
@@ -51,6 +53,7 @@ export default function MyReviewsPage() {
       throw new Error('생성에 실패했습니다');
     },
   });
+
   if (!shop.connected) return <div>connecting...</div>;
 
   const pathInfo: CreateReivewPathInfo = {
@@ -63,6 +66,7 @@ export default function MyReviewsPage() {
     score,
     memberId: shop.userID,
   };
+
   const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(event.target.value);
   };
@@ -77,6 +81,8 @@ export default function MyReviewsPage() {
   const submit = () => {
     if (content.length >= 10) {
       createReviewMutation.mutate();
+    } else {
+      textareaRef.current ? textareaRef.current.focus() : null;
     }
   };
 
@@ -84,32 +90,41 @@ export default function MyReviewsPage() {
     <main className="relative">
       <div className="flex justify-center text-lg font-medium p-2">리뷰쓰기</div>
       <CloseButton close={close} />
-      <BigStar
-        setStar={setScore}
-        star={score}
-      />
+      <div className="border-y-2">
+        <div className="flex justify-center text-lg font-medium pt-8">상품이 마음에 드셨나요?</div>
+        <div className="flex flex-row justify-center p-6">
+          <Star
+            setStar={setScore}
+            size="big"
+            star={score}
+          />
+        </div>
+      </div>
       <div className="relative flex flex-col gap-8 px-4 pt-4">
         <textarea
           className="border-2 overflow-hidden resize-none"
           maxLength={2000}
           onChange={handleChange}
           placeholder="리뷰를 작성해주세요. (최소 10자 이상)"
+          ref={textareaRef}
           rows={6}
           value={content}
         />
         <div className="absolute right-1 bottom-0 pr-4 text-gray-400">{content.length}/2000</div>
       </div>
-      {content.length < 10 && content.length !== 0 ? (
-        <div className="flex flex-row-reverse pr-4 text-xs text-gray-400">최소 10자 이상 입력해주세요!</div>
-      ) : (
-        <p> </p>
-      )}
+
+      <div
+        className="flex flex-row-reverse pr-4"
+        hidden={content.length >= 10 || content.length === 0}
+      >
+        <div className="text-gray-500"> 최소 10자 이상 입력해주세요!</div>
+        <div className="text-red-600/80">**</div>
+      </div>
       <ImageUploader
         setUploadImages={setUploadImages}
         uploadImages={uploadImages}
       />
-
-      <div className="grid grid-cols-2 justify-center text-lg font-medium place-content-around p-3 w-100% mb-4">
+      <div className="grid grid-cols-2 justify-center text-lg p-3 w-100% mb-4">
         <button
           className="border-2 border-gray-400/85 text-gray-400 p-2 m-2"
           onClick={close}
@@ -126,45 +141,5 @@ export default function MyReviewsPage() {
         </button>
       </div>
     </main>
-  );
-}
-
-interface StarProps {
-  star: number;
-  setStar: (star: number) => void;
-}
-
-function BigStar({ star, setStar }: StarProps) {
-  const StarArray = [1, 2, 3, 4, 5];
-
-  return (
-    <div className="border-y-2">
-      <div className="flex justify-center text-lg font-medium pt-8">상품이 마음에 드셨나요?</div>
-      <div className="flex flex-row justify-center p-6">
-        {StarArray.map((it) => (
-          <div key={it}>
-            <button
-              onClick={() => {
-                setStar(it);
-              }}
-              type="button"
-            >
-              <svg
-                fill="none"
-                height="50"
-                viewBox="0 0 16 16"
-                width="50"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M3.56795 14.943C3.18652 15.141 2.7537 14.794 2.83077 14.351L3.65096 9.62118L0.169623 6.26529C-0.155486 5.9513 0.0134919 5.37732 0.449276 5.31532L5.28935 4.61935L7.44752 0.29249C7.64219 -0.0974968 8.16888 -0.0974968 8.36355 0.29249L10.5217 4.61935L15.3618 5.31532C15.7976 5.37732 15.9666 5.9513 15.6405 6.26529L12.1601 9.62118L12.9803 14.351C13.0574 14.794 12.6246 15.141 12.2431 14.943L7.90405 12.6871L3.56795 14.943Z"
-                  fill={it <= star ? '#FBB230' : '#E1E1E1'}
-                />
-              </svg>
-            </button>
-          </div>
-        ))}
-      </div>
-    </div>
   );
 }
