7ae46adc992040b0473451ada9ae726097308bfe
wodnd0131 <wodnd0131@naver.com>
2024-05-29 09:07:05 +0900

    refactor: li 태그 에러 수정

--- apps/web/src/components/review/reply.tsx
+++ apps/web/src/components/review/reply.tsx
@@ -32,52 +32,54 @@ export default function Reply(props: ReplyItemProps) {
   const deleteReply = () => {};
 
   return (
-    <li
-      css={[
-        generateMarginCSS(style.margin),
-        generatePaddingCSS(style.padding),
-        generateBorderCSS(style.border, style.borderColor),
-        generateBorderRadiusCSS(style.borderRadius),
-        generateFontCSS(style.font),
-        generateShadowCSS(style.shadow, style.shadowColor),
-        css`
-          background-color: ${style.backgroundColor};
-          display: flex;
-          flex-direction: column;
-          gap: 8px;
-        `,
-      ]}
-    >
-      <div className="relative">
-        <div className="w-fit">
-          작성자 <span>{props.reply.nickname}</span>
-        </div>
-        <p className="text-left">{props.reply.content}</p>
-        {/*eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions --
+    <ul>
+      <li
+        css={[
+          generateMarginCSS(style.margin),
+          generatePaddingCSS(style.padding),
+          generateBorderCSS(style.border, style.borderColor),
+          generateBorderRadiusCSS(style.borderRadius),
+          generateFontCSS(style.font),
+          generateShadowCSS(style.shadow, style.shadowColor),
+          css`
+            background-color: ${style.backgroundColor};
+            display: flex;
+            flex-direction: column;
+            gap: 8px;
+          `,
+        ]}
+      >
+        <div className="relative">
+          <div className="w-fit">
+            작성자 <span>{props.reply.nickname}</span>
+          </div>
+          <p className="text-left">{props.reply.content}</p>
+          {/*eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions --
         This is intentional*/}
-        <div
-          className="absolute top-2 right-1 z-5"
-          hidden={props.reply.isMine}
-          onClick={(evt) => {
-            evt.stopPropagation();
-          }}
-        >
-          <button
-            className="border-b-2 border-gray-600/70 text-gray-700/90 mx-1"
-            onClick={edit}
-            type="button"
+          <div
+            className="absolute top-2 right-1 z-5"
+            hidden={props.reply.isMine}
+            onClick={(evt) => {
+              evt.stopPropagation();
+            }}
           >
-            수정
-          </button>
-          <button
-            className="border-b-2 border-gray-600/70 text-gray-700/90 mx-1"
-            onClick={deleteReply}
-            type="button"
-          >
-            삭제
-          </button>
+            <button
+              className="border-b-2 border-gray-600/70 text-gray-700/90 mx-1"
+              onClick={edit}
+              type="button"
+            >
+              수정
+            </button>
+            <button
+              className="border-b-2 border-gray-600/70 text-gray-700/90 mx-1"
+              onClick={deleteReply}
+              type="button"
+            >
+              삭제
+            </button>
+          </div>
         </div>
-      </div>
-    </li>
+      </li>
+    </ul>
   );
 }
