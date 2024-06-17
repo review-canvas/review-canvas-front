692a07b81534c79420cdc1c93fd09b30d89ed537
wodnd0131 <wodnd0131@naver.com>
2024-06-17 16:28:22 +0900

    feat:constant filter,order 선언

---
+++ apps/web/src/constants/list/filter/index.ts
@@ -0,0 +1,14 @@
+export const FILTER_OPTIONS = [
+  {
+    value: 'ALL',
+    label: '전체',
+  },
+  {
+    value: 'IMAGE_VIDEO',
+    label: '포토/동영상',
+  },
+  {
+    value: 'GENERAL',
+    label: '일반',
+  },
+];
---
+++ apps/web/src/constants/list/order/index.ts
@@ -0,0 +1,14 @@
+export const SORT_OPTIONS = [
+  {
+    value: 'LATEST',
+    label: '최신순',
+  },
+  {
+    value: 'HIGH_SCORE',
+    label: '평점높은순',
+  },
+  {
+    value: 'LOW_SCORE',
+    label: '평점낮은순',
+  },
+];
